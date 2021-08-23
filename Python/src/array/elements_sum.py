ae24aeaa23971d6cb0955a99804217befaad9d86
AlekseyGricanov <50859448+AlekseyGricanov@users.noreply.github.com>
2021-08-23 23:04:20 +0700

    Update test_elements_sum.py
    Правки импорта + формата

--- Python/src/array/elements_sum.py
+++ Python/src/array/elements_sum.py
@@ -1,7 +1,9 @@
-'''
+"""
 Найти в массиве натуральных чисел A сумму всех элементов, кратных параметру K.
 https://t.me/c/1533281926/13
-'''
+"""
+
+
 def sum_of_elements_that_are_multiples_of_k(arr, k):
     if k == 0 or len(arr) == 0:
         return
--- Python/test/array/test_elements_sum.py
+++ Python/test/array/test_elements_sum.py
@@ -1,23 +1,26 @@
 import unittest
-from src.array.elements_sum import sum_of_elements_that_are_multiples_of_k
+from Python.src.array.elements_sum import sum_of_elements_that_are_multiples_of_k
+
 
 class TestSumOfElementsThatAreMultiplesOfK(unittest.TestCase):
-  def setUp(self):
-    pass
+    def setUp(self):
+        pass
 
-  def test_zero_k(self):
-    self.assertIsNone(sum_of_elements_that_are_multiples_of_k([1, 2, 3], 0), 'При нулевом параметре K вернулся непустой результат')
+    def test_zero_k(self):
+        self.assertIsNone(sum_of_elements_that_are_multiples_of_k([1, 2, 3], 0),
+                          'При нулевом параметре K вернулся непустой результат')
 
-  def test_empty_arr(self):
-    self.assertIsNone(sum_of_elements_that_are_multiples_of_k([], 42), 'При пустом параметре arr вернулся непустой результат')
+    def test_empty_arr(self):
+        self.assertIsNone(sum_of_elements_that_are_multiples_of_k([], 42),
+                          'При пустом параметре arr вернулся непустой результат')
 
-  def test_k_is_equal_one(self):
-    self.assertEqual(sum_of_elements_that_are_multiples_of_k([1, 2, 3, 4, 5], 1), 15)
+    def test_k_is_equal_one(self):
+        self.assertEqual(sum_of_elements_that_are_multiples_of_k([1, 2, 3, 4, 5], 1), 15)
 
-  def test_real_results(self):
-    self.assertEqual(sum_of_elements_that_are_multiples_of_k([1, 2, 3, 4, 5], 2), 6)
-    self.assertEqual(sum_of_elements_that_are_multiples_of_k([1, 2, 3, 4, 5], 3), 3)
-    self.assertEqual(sum_of_elements_that_are_multiples_of_k([42, 72, 84], 42), 126)
+    def test_real_results(self):
+        self.assertEqual(sum_of_elements_that_are_multiples_of_k([1, 2, 3, 4, 5], 2), 6)
+        self.assertEqual(sum_of_elements_that_are_multiples_of_k([1, 2, 3, 4, 5], 3), 3)
+        self.assertEqual(sum_of_elements_that_are_multiples_of_k([42, 72, 84], 42), 126)
 
 if __name__ == "__main__":
   unittest.main()
