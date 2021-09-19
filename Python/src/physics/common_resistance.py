b6d32c5e2f45e49d134d329533d65eeb4594bbd3
Mikhail Perevalov <mixei87@mail.ru>
2021-09-19 11:50:30 +0700

    mixei87 5 complete

--- Python/src/physics/common_resistance.py
+++ Python/src/physics/common_resistance.py
@@ -3,9 +3,14 @@
 https://t.me/the_programmer_only_has_a_path/2
 https://t.me/c/1533281926/5
 '''
-def common_resistance (R1, R2, R3):
-  if R1<0 or R2<0 or R3<0:
-    return
-  if R1 == R2 == R3 == 0:
-    return 0
-  return R1 * R2 * R3 / (R1 + R2 + R3)
+
+
+def common_resistance(R1, R2, R3):
+    if R1 < 0 or R2 < 0 or R3 < 0:
+        return
+    if not R1 or not R2 or not R3:
+        return 0
+    sum = R1 + R2
+    mul = R1 * R2
+    sum = sum * R3 + mul
+    return mul * R3 / sum
--- Python/test/physics/test_common_resistance.py
+++ Python/test/physics/test_common_resistance.py
@@ -6,9 +6,6 @@ class TestCommonResistance(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_zero(self):
-        self.assertEqual(common_resistance(0, 0, 0), 0)
-
     def test_negative(self):
         self.assertIsNone(common_resistance(-1, 0, 0),
                           'При отрицательных аргументах вернулся непустой результат')
@@ -21,13 +18,22 @@ class TestCommonResistance(unittest.TestCase):
         self.assertIsNone(common_resistance(-0.0008, -9, -105.8),
                           'При отрицательных аргументах вернулся непустой результат')
 
+    def test_zero(self):
+        self.assertEqual(common_resistance(0, 5.4, 9), 0)
+        self.assertEqual(common_resistance(2, 0, 40), 0)
+        self.assertEqual(common_resistance(890, 10, 0), 0)
+        self.assertEqual(common_resistance(0, 0, 15), 0)
+        self.assertEqual(common_resistance(16.84, 0, 0), 0)
+        self.assertEqual(common_resistance(0, 100, 0), 0)
+        self.assertEqual(common_resistance(0, 0, 0), 0)
+
     def test_is_equal_one(self):
         self.assertEqual(common_resistance(1, 1, 1), 1/3)
 
     def test_real_results(self):
-        self.assertEqual(common_resistance(2, 7, 5), 5)
-        self.assertEqual(common_resistance(3, 0, 10), 0)
-        self.assertEqual(common_resistance(40, 0.2, 7.5), 60/47.7)
+        self.assertEqual(common_resistance(2, 7, 5), 70/59)
+        self.assertEqual(common_resistance(3, 300, 10), 300/131)
+        self.assertEqual(common_resistance(40, 0.2, 7.5), 120/619)
 
 
 if __name__ == "__main__":
