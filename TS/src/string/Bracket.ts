ee9b7ea01415b5f345f9eb22cb6da0fe25540985
Sergey Pecherkin <ahavando@gmail.com>
2021-10-02 13:10:12 +0700

    https://t.me/c/1533281926/19

---
+++ TS/src/string/Bracket.ts
@@ -0,0 +1,33 @@
+const brackets = {
+    curved: {
+        open: '(',
+        close: ')'
+    }
+}
+
+/**
+ * Написать метод, который вернёт ИСТИНА, если во входной строке круглые скобки содержат корректные пары.
+ * Например, "(123)", "(1(23(4))5)A(bc)", "(((())))"
+ * @param str 
+ * @returns
+ * @author PS11011010
+ * @description У нас только 1 вид скобок. Поэтому достаточно посчитать количество
+ * открывающих и закрывающих скобок. Их количество должно совпадать.
+ */
+const validCurvedBrackets: (str: string) => boolean = (str) => {
+    let openedBracketsCount = 0;
+    for(let i = 0; i < str.length; i++) {
+        if (str[i] === brackets.curved.open) {
+            openedBracketsCount++;
+        }
+        if (str[i] === brackets.curved.close) {
+            openedBracketsCount--;
+        }
+    }
+
+    return !openedBracketsCount;
+}
+
+export {
+    validCurvedBrackets
+}
