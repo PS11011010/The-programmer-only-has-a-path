24ef7203d3516b61610dc4990d1732c0d4201553
Vladimir <vovchik81@bk.ru>
2021-10-12 11:22:31 +0700

    huck811 https://t.me/c/1533281926/79

--- Python/src/string/number_of_plates.py
+++ Python/src/string/number_of_plates.py
@@ -1,4 +1,9 @@
-def number_of_plates(count):
+# -*- coding: utf-8 -*-
+__author__ = "Kasyanov Vladimir"
+__email__ = "vovchik81@bk.ru"
+
+
+def number_of_plates(count: int):
     """
     Дан многоквартирный дом. Получив общее количество квартир на вход функции, выдайте строку формата
     [0-A, 1-B, 2-C,...9-G], где 0-1 - это табличка с цифрой, а A-G - это количество табличек, требуемое
@@ -12,19 +17,26 @@ def number_of_plates(count):
     """
     numbers = {'0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0}
 
-    if count > 1:
-        for i in range(1, count + 1):
-            # Число - Строка - Итератор - Список
-            for sym in list(iter(str(i))):
-                numbers[sym] += 1
+    if not isinstance(count, int):
+        raise ValueError("Incorrect value!")
 
-    
+    if count <= 0:
+        return format_numbers(numbers)
+
+    for i in range(1, count + 1):
+        for j in str(i):
+            numbers[j] += 1
+
     return format_numbers(numbers)
 
-def format_numbers(numbers):
-    result = ''
-    for i in range(0, 10):
-        #Шаблоны строк - очень мощный инструмент. А еще нам незачем добавлять запятую и пробел в конце
-        result += '{0}-{1}{2}'.format(str(i), str(numbers.get(str(i))), '' if i == 9 else ', ')
+
+def format_numbers(numbers: dict) -> str:
+    """
+    :param dict numbers: {'0': 3, '1': 5, '2': 5, '3': 3, ... }
+    :return: '0-3, 1-5, 2-5, 3-3,...'
+    """
+    result = []
+    for number, count in numbers.items():
+        result.append(f'{number}-{count}')
     
-    return result
+    return ', '.join(result)
