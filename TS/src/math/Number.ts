f804c24f4dcf428dc6df3cacac98f0b6fa73c9f2
Sergey Pecherkin <ahavando@gmail.com>
2021-10-18 20:06:33 +0700

    https://t.me/c/1533281926/60

--- TS/src/math/Number.ts
+++ TS/src/math/Number.ts
@@ -1,3 +1,5 @@
+const digits = [...Array(10)].map((_, i) => i);
+
 /**
  * Реализовать функцию isOdd, которая вернёт ИСТИНА, если входное число нечётное.
  * Если число четное, нужно вернуть ЛОЖЬ.
@@ -9,6 +11,45 @@ const isOdd: (n: number) => boolean = (n) => {
     return !!(n % 2)
 }
 
+/**
+ * Реализовать функцию parseInt, которая пытается найти число в строке,
+ * разбирая все символы по-порядку. Процесс разбора не прекратится, пока
+ * не встретится невалидный символ или конец строки. Основание десятичное.
+ * 
+ * P.S. Переполнения могут быть. Один из тестов должен учитывать результат,
+ * который перевалил за максимальное значение целочисленной переменной.
+ * https://t.me/c/1533281926/60
+ * @param n 
+ * @author PS11011010
+ * @description Для каждого символа в строке, начиная слева выполняем проверку
+ * Если символ входит в набор допустимых символов (0-9),
+ * то смещаем 1 разряд результата (уvножаем на 10) и добавляем в конец текущее число,
+ * а вот, если попался невалидный символ, выходим из цикла.
+ * Перед выводом результата смотрим: если не было ни одного валидного символа - результат null
+ */
+const parseInt: (str: string) => number | null = (str) => {
+    const allowedSymbols: string[] = digits.map(digit => String(digit))
+
+    let result: number = 0;
+    let hasValidSymbols = false;
+    let symbol: string;
+
+    for (let i = 0; i < str.length; i++) {
+        symbol = str.charAt(i);
+
+        if (!allowedSymbols.includes(symbol)) {
+            break;
+        }
+        
+        hasValidSymbols = true;
+        result = result * 10 + (+symbol);
+    }
+
+    return hasValidSymbols ? result : null;
+}
+
 export {
-    isOdd
+    digits,
+    isOdd,
+    parseInt
 }
--- TS/test/math/Number.test.ts
+++ TS/test/math/Number.test.ts
@@ -1,4 +1,4 @@
-import { isOdd } from "../../src/math/Number";
+import { isOdd, parseInt } from "../../src/math/Number";
 
 describe('isOdd', () => {
     it('Even number', () => {
@@ -7,5 +7,45 @@ describe('isOdd', () => {
 
     it('Odd number', () => {
         expect(isOdd(1991)).toBe(true);
+    })
+})
+
+describe('parseInt', () => {
+    it('Empty data', () => {
+        expect(parseInt('')).toBe(null);
+    })
+
+    it('Incorrect data', () => {
+        expect(parseInt('foo')).toBe(null);
+        expect(parseInt('bar42')).toBe(null);
+    })
+
+    it('Correct data', () => {
+        expect(parseInt('1')).toBe(1)
+        expect(parseInt('42')).toBe(42)
+        expect(parseInt('999')).toBe(999)
+    })
+
+    /**
+     * У JavaScript максимальное целое число - это 1.7976931348623157e+308
+     * или
+     * 179 769 313 486 231 570 000 000 000 000 000 000 000 000 000 000
+     * 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000
+     * 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000
+     * 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000
+     * 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000
+     * 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000
+     * 000 000 000 000 000 000 000
+     * Тут 292 нуля
+     * Операции со значениями, которые превышают это число лучше не производить
+     * https://stackoverflow.com/questions/307179/what-is-javascripts-highest-integer-value-that-a-number-can-go-to-without-losin
+     */
+    it('Over big int', () => {
+        const data = '17976931348623157'.split('')
+            .concat([...Array(291)].map(() => '0'))
+            .concat(['1'])
+            .join('')
+
+        expect((parseInt(data) as number) <= Number.MAX_VALUE).toBe(true);
     })
 })
