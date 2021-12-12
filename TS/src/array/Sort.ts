e549b4dd74d5bae4b163a9fc627e8ac02bf30f3b
Sergey Pecherkin <ahavando@gmail.com>
2021-12-12 23:21:51 +0700

    https://t.me/c/1533281926/56

--- TS/src/array/Sort.ts
+++ TS/src/array/Sort.ts
@@ -10,6 +10,10 @@ import { reverseString } from "../string/Reversed"
  * ответ на вопрос: "А как мы будем это тестировать?"
  * Чтобы не запутаться во время написания тестов, я реализовал функцию перевода числа
  * из десятичной формы в бинарную.
+ * Теперь о сравнении. Больше будет то число, количество символов в котором больше.
+ * В случае идентичности же длинн следует вести посимвольное сравнение строк слева направо.
+ * 
+ * Подробнее о функции Array.sort в JavaScript https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
  */
 const sortBinaryNumbers: (data: string[]) => string[] = (data) => {
     return data
